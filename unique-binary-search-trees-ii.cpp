c377e2dacc9a54f4c7ecd1e8a1ecfb13d5dcf57b
yuhangtao <1102720350@qq.com>
2024-08-10 17:56:17 +0800

    update

--- unique-binary-search-trees-ii.cpp
+++ unique-binary-search-trees-ii.cpp
@@ -9,57 +9,33 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-using namespace std;
-#include<iostream>
-#include<vector>
-
-struct TreeNode {
-	int val;
-	TreeNode *left;
-	TreeNode *right;
-	TreeNode() : val(0), left(nullptr), right(nullptr) {}
-	TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
-	TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}	
-};
-
 class Solution {
 public:
-	vector<TreeNode*> generateTrees(int n) {
-		vector<TreeNode*> ans;
-		TreeNode *now = nullptr;
-		int size = 0;
-		ans.push_back(&TreeNode(1));
-		if (n == 1)
-		{
-			return ans;
-		}
-		for (int i = 2; i < n; i++) {
-			size = ans.size();
-			for (int j = 0; j < size; j++) {
-				TreeNode *newNode = new TreeNode(i);
-				newNode->left = ans[j];
-				ans.push_back(newNode);//需要浅拷贝二叉树才能成功运行
-				now = ans[j];
-				while (true)
-				{
-					if (i > now->val && now->right != nullptr) {
-						now = now->right;
-					}
-					else if (i > now->val && now->right == nullptr)
-					{
-						now->right = new TreeNode(i);
-						break;
-					}
-				}
-			}
-		}
-		return ans;
-	}
+    vector<TreeNode*> generateTrees(int n) {
+        if (!n) {
+            return {};
+        }
+        return func(1, n);
+    }
+    vector<TreeNode*> func(int begin, int end) {
+        if (begin > end) {
+            return { nullptr };
+        }
+        vector<TreeNode*> res;
+        vector<TreeNode*> vec_left;
+        vector<TreeNode*> vec_right;
+        for (int i = begin; i <= end; ++i) {
+            vec_left = func(begin, i - 1);
+            vec_right = func(i + 1, end);
+            for (TreeNode* left : vec_left) {
+                for (TreeNode* right : vec_right) {
+                    TreeNode* now = new TreeNode(i);
+                    now->left = left;
+                    now->right = right;
+                    res.emplace_back(now);
+                }
+            }
+        }
+        return res;
+    }
 };
-
-int main() {
-	vector<TreeNode*> ans;
-	Solution s;
-	ans = s.generateTrees(5);
-	cout << ans.size();
-}
---
+++ unique-binary-search-trees.cpp
@@ -0,0 +1,21 @@
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int numTrees(int n) {
+        vector<int> ls(n + 1, 0);
+        ls[0] = 1;
+        ls[1] = 1;
+        for (int i = 2; i <= n; ++i) {
+            for (int j = 1; j <= i; ++j) {
+                ls[i] += ls[j - 1] * ls[i - j];
+            }
+        }
+        return ls[n];
+    }
+};
+int main() {
+    Solution s;
+    s.numTrees(3);
+}
